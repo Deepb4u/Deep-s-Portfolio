f17c56dec7b21ed399a294fa30a17b2eb7434933
Deepb4u <deepbansode1@gmail.com>
2024-11-10 17:05:54 +0530

    initial commit

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -11,8 +11,8 @@ export default function FrontPage () {
                     <div className="col-lg-6 d-flex flex-column justify-content-center">
                         <h3 className="banner-1">Hi, I am</h3>
                         <h1 className="banner-2">Deep Bansode</h1>
-                        <p className="banner-p">Aspiring Full-Stack Web Developer | HTML, CSS, JavaScript, Node.js, Python | Passionate about Creating Innovative Web Solutions</p>
-                        <button className="btn btn-primary mt-2"><i className="fa-solid fa-cloud-arrow-down"></i> <a className="download_CV Anchor-tag" href="https://drive.google.com/file/d/1BBU-aUDIR4d9_6SbSTyoVAr-4LtSQrdR/view?usp=drivesdk">Download CV</a></button>
+                        <p className="banner-p">Aspiring Full-Stack Web Developer | "Frontend & UI/UX Developer | React, Angular, nodeJs & Full-Stack Projects | Robotic Intern | Passionate about Creating Innovative Web Solutions</p>
+                        <button className="btn btn-primary mt-2"><i className="fa-solid fa-cloud-arrow-down"></i> <a className="download_CV Anchor-tag" href="https://drive.google.com/file/d/1EZ3yPscPDSgdP1Anp2x0IHY1oPdNs9FF/view?usp=drive_link">Download CV</a></button>
                         <button className="btn btn-primary mt-2"><i className="fa-solid fa-phone"></i> Contact</button>
                     </div>
                     <div className="img col-lg-6">
